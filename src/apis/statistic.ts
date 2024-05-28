72ef57e5d0d3e09407b3aec0a1d5270f47919d28
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-05-28 17:46:19 +0900

    feat: 통계 관련 api 함수 구현 (#11)

--- src/apis/statistic.ts
+++ src/apis/statistic.ts
@@ -16,3 +16,78 @@ export const getStatisticsDay = async ({accessToken, projectId}:{accessToken: st
     console.error(error);
   }
 };
+
+export const getStatisticsMonth = async ({accessToken, projectId}:{accessToken: string, projectId: number}) => {
+  try{
+    const response = await axios.get(`${baseURL}/projects/${projectId}/statistics/month`, 
+    {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    return response.data;
+
+  }catch(error){
+    console.error(error);
+  }
+};
+
+export const getStatisticsAssignee = async ({accessToken, projectId}:{accessToken: string, projectId: number}) => {
+  try{
+    const response = await axios.get(`${baseURL}/projects/${projectId}/statistics/assignee`, 
+    {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    return response.data;
+
+  }catch(error){
+    console.error(error);
+  }
+};
+
+export const getStatisticsReporter = async ({accessToken, projectId}:{accessToken: string, projectId: number}) => {
+  try{
+    const response = await axios.get(`${baseURL}/projects/${projectId}/statistics/reporter`, 
+    {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    return response.data;
+
+  }catch(error){
+    console.error(error);
+  }
+};
+
+export const getStatisticsStatus = async ({accessToken, projectId}:{accessToken: string, projectId: number}) => {
+  try{
+    const response = await axios.get(`${baseURL}/projects/${projectId}/statistics/status`, 
+    {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    return response.data;
+
+  }catch(error){
+    console.error(error);
+  }
+};
+
+export const getStatisticsPriority = async ({accessToken, projectId}:{accessToken: string, projectId: number}) => {
+  try{
+    const response = await axios.get(`${baseURL}/projects/${projectId}/statistics/priority`, 
+    {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    return response.data;
+
+  }catch(error){
+    console.error(error);
+  }
+};
